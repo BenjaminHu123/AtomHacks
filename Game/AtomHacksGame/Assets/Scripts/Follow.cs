88ca62474169630c9d4c55b79e1f3bc8d05d9f34
Catherine-Wu <34039684+Catherine-Wu@users.noreply.github.com>
2019-03-30 14:39:53 -0400

    blep

---
+++ Game/AtomHacksGame/Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    public GameObject PlayerKnight;
+
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, PlayerKnight.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, PlayerKnight.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
