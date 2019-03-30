d2b16617d031b923044392efdfb287eeb6e62abe
Xiaolong Gao <PokemonDeath@Xiaolong-no-MacBook-Pro.local>
2019-03-30 15:29:19 -0400

    xiaolong's

---
+++ Game/AtomHacksGame/Assets/Scripts/spawnSans.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnSans : MonoBehaviour
+{
+    public GameObject sans;
+    private bool sansActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sans.SetActive(false);
+    }
+
+    void Updtate()
+    {
+
+    }
+    // Update is called once per frame
+    public void sansAction()
+    {
+        sans.SetActive(true);
+    }
+}
