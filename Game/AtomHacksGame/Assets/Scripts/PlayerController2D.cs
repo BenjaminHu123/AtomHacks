645abd22a4f35fce78ca57262301b0df819ab673
Caroline Huang <huangc@bxscience.edu>
2019-03-31 18:42:40 -0400

    final update

--- Game/AtomHacksGame/Assets/Scripts/PlayerController2D.cs
+++ Game/AtomHacksGame/Assets/Scripts/PlayerController2D.cs
@@ -40,21 +40,21 @@ public class PlayerController2D : MonoBehaviour {
         else {
             isGrounded = false;
         }
-        float horizontal = Input.GetAxis("Horizontal");
-        Flip(horizontal);
+        //float horizontal = Input.GetAxis("Horizontal");
+        //Flip(horizontal);
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             myRigidBody.velocity = new Vector2(runSpeed, myRigidBody.velocity.y);
             if (isGrounded)
                 animator.Play("WalkKnight");
-            //spriteRenderer.flipX = false;
+            spriteRenderer.flipX = false;
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             myRigidBody.velocity = new Vector2(-1 * runSpeed, myRigidBody.velocity.y);
             if (isGrounded)
                 animator.Play("WalkKnight");
-           // spriteRenderer.flipX = true;
+            spriteRenderer.flipX = true;
         }
         else if (Input.GetKey(KeyCode.Z) && isGrounded) {
             animator.SetTrigger("attack");
@@ -72,7 +72,7 @@ public class PlayerController2D : MonoBehaviour {
         if(!isGrounded && myRigidBody.velocity.y < 0)
             animator.Play("FallKnight");
     }
-    private void Flip(float horizontal)
+    /*private void Flip(float horizontal)
     {
         if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
@@ -81,7 +81,7 @@ public class PlayerController2D : MonoBehaviour {
             theScale.x *= -1;
             transform.localScale = theScale;
         }
-    }
+    }*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
